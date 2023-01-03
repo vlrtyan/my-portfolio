17a17e403597744fd6ecc4faf10f8b5318feb7ba
Valeria Tyan <vlrtyan@yandex.ru>
2023-01-03 21:56:30 +0300

    fix: smooth scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,29 @@
 import './index.scss';
 
 const roles = [
-  'и футбольная фанатка',
-  'и люблю stand-up',
-  'и путешественница',
-  'и киноманка',
+  "футбольная фанатка",
+  "киноманка",
+  "роль 3",
+  "роль 4"
 ];
+
+const role = document.querySelector('.role');
 
 function getRandomElement(arr) {
   const randIndex = Math.floor(Math.random() * arr.length);
   return arr[randIndex];
 }
 
-// eslint-disable-next-line no-undef
-const role = document.querySelector('.role');
-
 function changeRole() {
   role.textContent = getRandomElement(roles);
 }
 
 setInterval(changeRole, 5000);
+
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.navigation__link')) {
+    event.preventDefault();
+    let element = document.getElementById(event.target.dataset.target);
+    element.scrollIntoView({behavior: "smooth"});
+  }
+});
